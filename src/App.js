51dab94f9f5ae86d5cbf1770a06c76c950ebdb8c
MariaRosenholm <maria.rosenholm@gmail.com>
2022-01-23 15:13:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+      const fethches = res.data.results.map((p) =>
+        axios.get(p.url).then((res) => res.data)
+      );
+
+      Promise.all(fethches).then((data) => {
+        setPokemons(data);
+        setIsLoading(false);
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Navbar
+        bg="dark"
+        variant="dark"
+        className="back"
+        style={{ padding: "2rem" }}
+      >
+        <Container>
+          <Navbar.Brand href="#home">Pokemons</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row
+          xs={2}
+          md={4}
+          className="justify-content-between d-flex my-5 gap-5"
         >
-          Learn React
-        </a>
-      </header>
+          {pokemons.map((p) => (
+            <Col>
+              <Card bg="dark" text="light">
+                <Card.Body>
+                  <Card.Text style={{ color: "white" }}>
+                    {p.name.substring(0, 1).toUpperCase() + p.name.substring(1)}
+                  </Card.Text>
+                </Card.Body>
+                <Card.Img
+                  style={{ height: "15rem", padding: "2rem" }}
+                  variant="bottom"
+                  src={p.sprites.other.dream_world.front_default}
+                />
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
